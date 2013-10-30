755332d6b28233560204ba1717bbf815c6ce5a98
gabrielmarin <gabrielhenriquemarin@gmail.com>
2013-10-29 22:34:11 -0200

    gkjhkj

--- SistemaPassagensAereas/src/java/Model/Aeroporto.java
+++ SistemaPassagensAereas/src/java/Model/Aeroporto.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 
 
 @Entity
-public class Aeroporto implements Serializable {
+public class Aeroporto {
 
     
     @Id
--- SistemaPassagensAereas/src/java/Model/Compra.java
+++ SistemaPassagensAereas/src/java/Model/Compra.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class Compra implements Serializable {
+public class Compra {
 
     @Id
     @GeneratedValue
